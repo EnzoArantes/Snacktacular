ce5e335e1ed06aed99ab45fcf899d274795f0560
Zo-hack <enzoarantes2000@gmail.com>
2025-03-28 17:43:59 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Enzo Arantes on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
